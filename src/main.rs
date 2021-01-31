b72282099c615a6407fed129e6cee813136a085d
Ryan P. Brewster <RyanPBrewster@gmail.com>
2021-01-31 16:19:19 -0500

    simplify

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+use fst::IntoStreamer;
+use fst::{automaton::Subsequence, Streamer};
 use std::{
     io::{stdin, stdout, Write},
     time::Instant,
 };
 use termion::event::{Event, Key};
 use termion::input::{MouseTerminal, TermRead};
-use termion::raw::IntoRawMode;
-use fst::{IntoStreamer};
-use fst::{Streamer, automaton::Levenshtein};
+use termion::{cursor::Goto, raw::IntoRawMode};
 
 const WORDS: &str = include_str!("../data/words_alpha.txt");
 const MAX_MATCHES: u16 = 8;
@@ -15,16 +15,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let set = fst::Set::from_iter(WORDS.lines())?;
 
     let stdin = stdin();
-    let mut stdout = MouseTerminal::from(stdout().into_raw_mode().unwrap());
+    let mut stdout = MouseTerminal::from(stdout().into_raw_mode()?);
 
     let mut query = String::new();
 
-    write!(stdout, "{}", termion::clear::All).unwrap();
-    stdout.flush().unwrap();
-    for c in stdin.events() {
-        let evt = c.unwrap();
-        match evt {
-            Event::Mouse(_) | Event::Unsupported(_) => {}
+    write!(stdout, "{}", termion::clear::All)?;
+    stdout.flush()?;
+    for evt in stdin.events() {
+        match evt? {
             Event::Key(key) => {
                 match key {
                     Key::Esc => break,
@@ -36,29 +34,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     }
                     _ => {}
                 };
-                write!(stdout, "{}", termion::clear::All).unwrap();
-                let start = Instant::now();
-                let lev = Levenshtein::new(&query, 1)?;
-                let mut stream = set.search(lev).into_stream();
-                let mut i = 0;
-                while let Some(key) = stream.next() {
-                    write!(stdout, "{}{}", termion::cursor::Goto(1, 2 + i), std::str::from_utf8(key)?)?;
-                    i += 1;
-                    if i > MAX_MATCHES {
-                        break;
-                    }
-                }
-                write!(
-                    stdout,
-                    "{}{}us",
-                    termion::cursor::Goto(1, 3 + i),
-                    start.elapsed().as_micros()
-                )
-                .unwrap();
-                write!(stdout, "{}{}", termion::cursor::Goto(1, 1), query).unwrap();
+            }
+            _ => {}
+        }
+        write!(stdout, "{}", termion::clear::All)?;
+        let start = Instant::now();
+        let mut stream = set.search(Subsequence::new(&query)).into_stream();
+        let mut i = 0;
+        while let Some(key) = stream.next() {
+            write!(stdout, "{}{}", Goto(1, 2 + i), std::str::from_utf8(key)?)?;
+            i += 1;
+            if i > MAX_MATCHES {
+                break;
             }
         }
-        stdout.flush().unwrap();
+        let elapsed = start.elapsed();
+        write!(stdout, "{}{}us", Goto(1, 3 + i), elapsed.as_micros())?;
+        write!(stdout, "{}{}", Goto(1, 1), query)?;
+        stdout.flush()?;
     }
     Ok(())
 }
